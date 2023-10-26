ce0b1b615d5588217ab8ac6ea7b03c7cc6932ca9
jeong922 <hwajeong922@naver.com>
2023-10-26 14:04:02 +0900

    client - react router 설정

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './page/Home';
+import MyTweets from './page/MyTweets';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: '/:username',
+        element: <MyTweets />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
---
+++ src/page/MyTweets.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function MyTweets() {
+  return <div>MyTweets</div>;
+}
